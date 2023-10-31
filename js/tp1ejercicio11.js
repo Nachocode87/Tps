86caa6c8824a883cd5848c7640270b4b6ad8825a
nachocode <lic.medinajimenez@gmail.com>
2023-10-31 19:30:58 -0300

    ultimos cambios

--- js/tp1ejercicio11.js
+++ js/tp1ejercicio11.js
@@ -1,12 +1,14 @@
 let num = parseInt(window.prompt("escriba una numero"));
 
-
+document.write(`el numero ${num} es divisible por `)
 if (num%2 === 0 && num%3 === 0 && num%5 === 0 && num%7 === 0 ) {
-  document.write(`el numero ${num} es divisible por 2, 3, 5, y 7`)  
+  document.write(`2, 3, 5, y 7`)  
 } else if (num%2 === 0 && num%3 === 0 && num%5 === 0) {
-  document.write(`el numero ${num} es divisible por 2, 3,y por 5`)  
+  document.write(`2, 3, y por 5`)  
+} else if (num%2 === 0 && num%5 === 0) {
+  document.write(`2, y por 5`)  
 } else if (num%2 === 0 && num%3 === 0) {
-  document.write(`el numero ${num} es divisible por 2, y por 3`)  
+  document.write(`2 y por 3`)  
 } else if (num%2 === 0) {
-  document.write(`el numero ${num} es divisible por 2`)  
+  document.write(`2`)  
 }
--- js/tp1ejercicio9.js
+++ js/tp1ejercicio9.js
@@ -1,15 +1,15 @@
-let frase = window.prompt('escriba una frase');
-
+let frase = window.prompt("escriba una frase");
 
 for (let index = 0; index < frase.length; index++) {
-    
-
-    if(frase[index] === 'a' || frase[index] === 'e' || frase[index] === 'i' || frase[index] === 'o' || frase[index] === 'u') {
-        
-        document.write(frase[index])
-        
-    } else {
-        document.write('  ')
-    }
-    
+  if (
+    frase[index] === "a" ||
+    frase[index] === "e" ||
+    frase[index] === "i" ||
+    frase[index] === "o" ||
+    frase[index] === "u"
+  ) {
+    document.write(frase[index] + " ");
+  } else {
+    document.write(" ");
+  }
 }
